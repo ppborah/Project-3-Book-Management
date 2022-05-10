0b1b7546c97c91ec9915464fcd79705600b94f8a
Prasurjya Pran Borah <96339995+ppborah@users.noreply.github.com>
2022-05-10 23:20:43 +0530

    updated userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,41 +1,48 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    enum: ["Mr", "Mrs", "Miss"],
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: Number,
-    required: true,
-    length: 10,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 8,
-    maxLength: 15,
-  },
-  address: {
-    street: String,
-    city: String,
-    pincode: {
+const userSchema = new mongoose.Schema(
+  {
+    title: {
       type: String,
-      length: 6,
-    }
-  }
-}, { timestamps: true })
-
+      required: true,
+      trim: true,
+      enum: ["Mr", "Mrs", "Miss"],
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      trim: true,
+      length: 10,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 15,
+    },
+    address: {
+      street: { type: String, trim: true },
+      city: { type: String, trim: true },
+      pincode: {
+        type: String,
+        trim: true,
+        length: 6,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
