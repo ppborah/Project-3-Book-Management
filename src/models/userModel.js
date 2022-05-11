c3c3587188e01d9d48a3a0936f6bfa94ec83aac7
Prasurjya Pran Borah <96339995+ppborah@users.noreply.github.com>
2022-05-11 17:14:11 +0530

    updated userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema(
       trim: true,
     },
     phone: {
-      type: Number,
+      type: String,
       required: true,
       trim: true,
       length: 10,
